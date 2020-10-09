f77f98d7d7992a5410871e3c1a3e2562e66f554a
aboutindracom <me@aboutindra.com>
2020-10-09 17:28:52 +0700

    Add endpoint update

--- bin/app/absent.js
+++ bin/app/absent.js
@@ -5,9 +5,9 @@ const app = express.Router();
 app.post("/insert",async (req, res) => {
     await absentHandlers.InsertDataAbsent(req, res)
 });
-// app.get("/find",async (req, res) => {
-//     await studentHandlers.FindStudent(req, res)
-// });
+app.get("/update",async (req, res) => {
+    await absentHandlers.UpdatePresentStudent(req, res)
+});
 // app.put("/",async (req, res) => {
 //     await studentHandlers.UpdateStudent(req, res)
 // });
